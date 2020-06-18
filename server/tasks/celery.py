a297b3dc8d983f258d6e3b8aa2c3a00b61b78c74
Samantha Finnigan <1038320+sjmf@users.noreply.github.com>
2020-06-18 22:29:36 +0100

    ⚠️ On error in celery task, raise it to trigger the FAILED state

--- server/tasks/celery.py
+++ server/tasks/celery.py
@@ -17,71 +17,63 @@ celery_app = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BAC
 def celery_model_get_bau(self, landscape_id):
 
     self.update_state(state='PROGRESS', meta={'status': 'Initialising'})
-    try:
-        # Initialise crop model
-        model = CropModel()
-        model.set_landscape_id(int(landscape_id))
-        model.initialise_model()
 
-        self.update_state(state='PROGRESS', meta={'status': 'Running'})
-        model.run_model()
+    # Initialise crop model
+    model = CropModel()
+    model.set_landscape_id(int(landscape_id))
+    model.initialise_model()
 
-        result = model.to_dict()
+    self.update_state(state='PROGRESS', meta={'status': 'Running'})
+    model.run_model()
 
-        #self.update_state(state='SUCCESS')
-        log.info(result)
+    result = model.to_dict()
 
-        return {'result': result}
+    #self.update_state(state='SUCCESS')
+    log.info(result)
 
-    except CropModelException as e:
-        self.update_state(state='FAILED')
-        return {'result': str(e)}
+    return {'result': result}
 
 
 @celery_app.task(bind=True, track_started=True, name='celery_model_run')
 def celery_model_run(self, landscape_id, data):
 
     self.update_state(state='PROGRESS', meta={'status': 'Initialising'})
-    try:
-        # Initialise crop model
-        model = CropModel()
-        model.set_landscape_id(int(landscape_id))
-        model.initialise_model()
 
-        # Extract parameters from strings and build a list to pass to CropModel
-        max_crops = len(model.cropAreasBAU)
-        crop_areas = []
-        for i in range(0, max_crops):
-            crop = model.get_crop_string(i).lower().split(' ')[0]
-            area = float(data[crop])
-            # log.info("{}={}".format(crop, area))
-            crop_areas.append(area)
+    # Initialise crop model
+    model = CropModel()
+    model.set_landscape_id(int(landscape_id))
+    model.initialise_model()
 
-        max_livestock = len(model.livestockNumbersBAU)
-        livestock_areas = []
-        for i in range(0, max_livestock):
-            livestock = model.get_livestock_string(i).lower()  # .split(' ')[0]
-            area = int(data[livestock])  # TODO: This will likely need to change to type float
-            # log.info("{}={}".format(livestock, area))
-            livestock_areas.append(area)
+    # Extract parameters from strings and build a list to pass to CropModel
+    max_crops = len(model.cropAreasBAU)
+    crop_areas = []
+    for i in range(0, max_crops):
+        crop = model.get_crop_string(i).lower().split(' ')[0]
+        area = float(data[crop])
+        # log.info("{}={}".format(crop, area))
+        crop_areas.append(area)
 
-        # set_crop_areas takes an ordered list of float point numbers
-        model.set_crop_areas(crop_areas)
-        model.set_livestock_areas(livestock_areas)
+    max_livestock = len(model.livestockNumbersBAU)
+    livestock_areas = []
+    for i in range(0, max_livestock):
+        livestock = model.get_livestock_string(i).lower()  # .split(' ')[0]
+        area = int(data[livestock])  # TODO: This will likely need to change to type float
+        # log.info("{}={}".format(livestock, area))
+        livestock_areas.append(area)
 
-        self.update_state(state='PROGRESS', meta={'status': 'Running'})
-        model.run_model()
+    # set_crop_areas takes an ordered list of float point numbers
+    model.set_crop_areas(crop_areas)
+    model.set_livestock_areas(livestock_areas)
 
-        result = model.to_dict()
+    self.update_state(state='PROGRESS', meta={'status': 'Running'})
+    model.run_model()
 
-        #self.update_state(state='SUCCESS')
-        log.info(result)
+    result = model.to_dict()
 
-        return {'result': result}
+    #self.update_state(state='SUCCESS')
+    log.info(result)
 
-    except CropModelException as e:
-        self.update_state(state='FAILED')
-        return {'result': str(e)}
+    return {'result': result}
 
 
 if __name__ == "__main__":
